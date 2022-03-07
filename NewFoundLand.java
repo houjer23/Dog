11b759cc53c4e00ff1ea0fd31e8cf5c0f24abd6d
houjer23 <60145875+houjer23@users.noreply.github.com>
2022-03-07 15:41:23 -0500

    Update NewFoundLand.java

--- NewFoundLand.java
+++ NewFoundLand.java
@@ -1,17 +1,29 @@
+/**
+* This class is a sub class of Dog
+*
+* @author Jerry Hou
+* @since 1.0
+*/
+
 public class NewFoundLand extends Dog {
+	
+	// constructor
 	public NewFoundLand(String name, int birth) {
 		super(name, birth);
 	}
 	
+	// swim method specific to New Found Land
 	public void swim() {
 		System.out.println(super.getName() + " is swimming");
 	}
 	
+	// toString method used for printing
 	public String toString() {
 		return super.toString();
 	}
 	
+	// bark method replace the method in the super class
 	public void bark(String sound) {
 		System.out.println(sound);
 	}
-}
+} // end of the Dog class
