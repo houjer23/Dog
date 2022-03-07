9b37bccff941a8962f799b9188112e8ae8ee1314
houjer23 <60145875+houjer23@users.noreply.github.com>
2022-03-07 15:36:24 -0500

    Update Tester.java

--- Tester.java
+++ Tester.java
@@ -1,12 +1,22 @@
+/**
+* This class creates different types of doges and arrage it in the list, arrange it in the ArrayList, and print the ArrayList
+*
+* @author Jerry Hou
+* @since 1.0
+*/
+
 import java.util.ArrayList;
 public class Tester {
+	// where is code runs
 	public static void main(String args[]) {
+		// constructing different types of dogs
 		Dog d1 = new Dog("Bella", 10);
 		Dog d2 = new Dog ("Luna", 4);
 		ShihTzu s1 = new ShihTzu("Charlie", 5);
 		ShihTzu s2 = new ShihTzu("Lucy", 8);
 		NewFoundLand n1 = new NewFoundLand("Cooper", 1);
 		NewFoundLand n2 = new NewFoundLand("Max", 3);
+		// trying out different methods of ArrayList
 		ArrayList<Dog> dogs = new ArrayList<>();
 		dogs.add(d1);
 		dogs.add(d2);
@@ -19,12 +29,13 @@ public class Tester {
 		dogs.remove(n1);
 		dogs.set(3, new Dog("Daisy", 5));
 		printDogs(dogs);
-	}
+	} // end of public static void main
+	// this is the method that print the ArrayList nicely
 	public static void printDogs(ArrayList<Dog> dogs) {
 		System.out.println();
 		for (int i = 0; i < dogs.size(); i ++) {
 			System.out.println(dogs.get(i));
 		}
 		System.out.println();
-	}
-}
+	} // end of print dogs method
+} // end of the Tester class
