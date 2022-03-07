e4cb4ef058fc873775e04434b9e322c3baa1f307
houjer23 <60145875+houjer23@users.noreply.github.com>
2022-03-07 15:39:37 -0500

    Update Dog.java

--- Dog.java
+++ Dog.java
@@ -1,30 +1,43 @@
+/**
+* This class provides general properties of Dog
+*
+* @author Jerry Hou
+* @since 1.0
+*/
+
 public class Dog {
 	
 	private String name;
 	private int birth;
 	
+	// constructor
 	public Dog(String name, int birth) {
 		this.name = name;
 		this.birth = birth;
 	}
 	
+	// eat method
 	public void eat() {
 		System.out.println(name + " is eating");
 	}
 	
+	// sleep method
 	public void sleep() {
 		System.out.println(name + " is sleeping");
 	}
 	
+	// bark method
 	public void bark() {
 		System.out.println("woof");
 	}
 	
+	// get name method -- used for getting variables
 	public String getName() {
 		return name;
 	}
 	
+	// toString metod -- used for printing
 	public String toString() {
 		return "Name: " + name + ",     " + "Birth: " + birth;
 	}
-}
+} // end of the Dog class
